19ca351a069612a59de772ea03c7839226a7433e
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-13 17:27:42 -0300

    adding migrations for database

--- backend/database/migrations/2023_03_13_194344_create_products_table.php
+++ backend/database/migrations/2023_03_13_194344_create_products_table.php
@@ -11,12 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('value');
+            $table->float('discount_value', 4, 2);
             $table->timestamps();
         });
     }
@@ -26,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
