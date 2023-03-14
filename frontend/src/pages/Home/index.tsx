b47b8801361829a70d0988893f3cee1a76a75350
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-14 18:04:47 -0300

    Adding Home stylization

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,109 @@
+import "./styles.css";
+
+function Home() {
+    return (
+        <main>
+            <section>
+                <ul className="list-products">
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+
+                    <li>
+                        <img src="http://placeimg.com/640/480/business" alt="" />
+
+                        <strong>Tenis legal</strong>
+                        <span>R$130,30</span>
+
+                        <button>Adicionar ao Carrinho</button>
+                    </li>
+
+                </ul>
+            </section>
+        </main>
+    );
+}
+
+export default Home;
