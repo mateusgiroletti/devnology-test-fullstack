608d5c7d909ecf0116debde53592fd53b7921dfd
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-14 10:54:55 -0300

    Adding user create and login

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $newUser = [
+            'name' => $request['name'],
+            'email' => $request['email'],
+            'password' => bcrypt($request['password']),
+        ];
+
+        $user = User::create($newUser);
+
+        auth()->login($user);
+        $token = $user->createToken('JWT');
+
+        return response()->json($token->plainTextToken);
+    }
+}
