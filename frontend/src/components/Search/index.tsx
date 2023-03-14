1ad2a9f464f583e16025d7faa4e1422338526d42
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-14 18:04:27 -0300

    Adding Search stylization

---
+++ frontend/src/components/Search/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+function Search() {
+    return (
+        <div className="container-search">
+            <input type="text" placeholder="Digite para pesquisar"/>
+        </div>
+    );
+}
+
+export default Search;
