4b7a8b34fc8f931b6e8db5812352297d31de11e8
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-14 10:55:06 -0300

    Adding login

---
+++ backend/app/Http/Controllers/SessionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionController extends Controller
+{
+    public function store(Request $request)
+    {
+        $userLogin = [
+            'email' => $request->email, 'password' => $request->password
+        ];
+
+        if (Auth::attempt($userLogin)) {
+            $user = Auth::user();
+            $token = $user->createToken('JWT');
+
+            return response()->json($token->plainTextToken);
+        }
+
+        return response()->json(
+            [
+                'erro' => true,
+                'message' => 'user invalid'
+            ],
+            401
+        );
+    }
+}
