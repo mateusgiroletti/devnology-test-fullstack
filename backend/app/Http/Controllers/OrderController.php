21c140ff272e24b5fcc34a97cd216d657987eab5
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-03-13 21:52:08 -0300

    create route and controller to add a new order

---
+++ backend/app/Http/Controllers/OrderController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class OrderController extends Controller
+{
+    public function store(Request $request)
+    {
+        $order = $request->order;
+
+        $orderId = DB::table('orders')->insertGetId($order);
+
+        $products = $request->products;
+        foreach ($products as $product) {
+            $dataProduct = [
+                'name' => $product['name'],
+                'value' => $product['value'],
+                'discount_value' => $product['discount_value'] ?? null
+            ];
+
+            $productId = DB::table('products')->insertGetId($dataProduct);
+
+            $orderProduct = [
+                'order_id' => $orderId,
+                'product_id' => $productId,
+                'quantity' => $product['quantity']
+            ];
+
+            DB::table('orders_products')->insert($orderProduct);
+        }
+        return response()->json($request);
+    }
+}
