d80155a69190885e2a41b7613f137c2fd57bcf31
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-09-04 14:35:20 -0300

    Tests for user create

---
+++ backend/tests/Feature/CreateUserTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class CreateUserTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_register_user_and_return_errors_validation(): void
+    {
+        $userData = [
+            'name' => '',
+            'email' => '',
+            'password' => '',
+        ];
+
+        $response = $this->json('POST', '/api/user', $userData);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'success' => false,
+            'message' => 'Validation errors',
+            'data' => [
+                'name' => [
+                    'The name field is required'
+                ],
+                'email' => [
+                    'The email field is required'
+                ],
+                'password' => [
+                    'The password field is required'
+                ]
+            ]
+        ]);
+    }
+
+    public function test_register_user_and_return_token_and_email(): void
+    {
+        $userData = [
+            'name' => 'testing app',
+            'email' => 'testingapp@test.com',
+            'password' => bcrypt('testing'),
+        ];
+
+        $response = $this->json('POST', '/api/user', $userData);
+
+        $response->assertStatus(200);
+        $response->assertJsonStructure(['email']);
+        $response->assertJsonStructure(['token']);
+    }
+}
